621627f07d6b6c2577a6d32923bf6143831c7f8f
simonezanfagna <zanfagnasimone@live.it>
2020-02-23 22:53:32 +0100

    foto copertina

---
+++ resources/views/contatti.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.public')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h1 class="mt-3">Contattaci</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <form class="" action="{{route('contattiStore')}}" method="post">
+
+        </form>
+      </div>
+    </div>
+  </div>
+
+@endsection
